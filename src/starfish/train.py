2511c9a1a28e116034c40afe60f7238f876e7814
Lucia Gordon <luciagordon@g.harvard.edu>
2025-01-20 15:18:05 -0500

    Added profiling and logging

--- src/starfish/train.py
+++ src/starfish/train.py
@@ -1,26 +1,34 @@
 import hydra
 from omegaconf import DictConfig
 from hydra.utils import instantiate
+from torch.profiler import profile, tensorboard_trace_handler, ProfilerActivity
+from pathlib import Path
+from luguru import logger
 import torch
 import numpy as np
 import random
 import os
-from pathlib import Path
 
 # Ensure reproducibility by setting seeds for random number generation
-torch.manual_seed(409)
-np.random.seed(409)
-random.seed(409)
+torch.manual_seed(0)
+np.random.seed(0)
+random.seed(0)
 
-# Set CuBLAS workspace configuration for deterministic behavior
-os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"  # sets CuBLAS workspace configuration for deterministic behavior
+torch.backends.cudnn.deterministic = True # ensures that the CUDA backend produces deterministic results
+torch.backends.cudnn.benchmark = False # disables CuDNN benchmarking, which can introduce non-deterministic behavior
 
 config_path = str(Path(__file__).resolve().parent.parent.parent / "configs")
 
 @hydra.main(config_path=config_path, config_name="main_config", version_base="1.2")
 def train(cfg: DictConfig):
+    profiling = cfg.profiling
+
+    logger.info(profiling)
+    if profiling:
+        prof = profile(activities=[ProfilerActivity.CPU], on_trace_ready=tensorboard_trace_handler("profiling"))
+        prof.start()
+
     # 1. Instantiate the data module
     data_module = instantiate(cfg.data)
 
@@ -32,10 +40,15 @@ def train(cfg: DictConfig):
     trainer = instantiate(cfg.trainer, logger=logger)  # callbacks=[early_stopping], logger=True)
 
     # 4. Train the model
+    logger.info("\nTraining the model...")
     trainer.fit(model, data_module)
 
+    if profiling:
+        prof.stop()
+        print(prof.key_averages(group_by_input_shape=True).table(sort_by="cpu_time_total", row_limit=30))
+
     # 5. Test the model
-    print("\nTesting the model...")
+    logger.info("\nTesting the model...")
     trainer.test(model, data_module)
 
 if __name__ == "__main__":
